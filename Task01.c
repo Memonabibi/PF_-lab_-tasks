cf4b59fc47221bfac2b7d47792b29e26e5e6f011
Memonabibi <mona.sandhu888@gmail.com>
2026-09-13 06:49:21 +0500

    Create Task01.c

---
+++ Task01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int units = 650;
+
+    if (units > 500) {
+        printf("High Usage");
+    } else {
+        printf("Normal Usage");
+    }
+
+    return 0;
+}
